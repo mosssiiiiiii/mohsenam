002141afb8e4288c345a1aa210dc70061f6eaa0c
m.haghighakthah <haghighatkhah.mohsen@yahoo.com>
2020-08-09 15:32:54 +0430

    Providing home page structore for develop

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,31 @@ import { Link } from "gatsby"
 import "./../styles/home.scss"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <div className="container">
-       <SEO title="Mohsen Haghighatkhah Portfolio" />
-    <div className="row">
-      <div className='box'></div>
-      <h1 className="name">Mohsen Haghighatkhah</h1>
-      <p className="mode">Frontend developer</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
-      <Link to="/aboutMe/">About me</Link> <br />
+const Home = () => {
+  const height = window.innerHeight
+  console.log(height)
+  return (
+    <div style={{ maxHeight: height }} className="container">
+      <SEO title="Mohsen Haghighatkhah Portfolio" />
+      <div className="row">
+        <div className="box-wrap">
+          <div className="box"></div>
+          <div>
+            <h1 className="name">Mohsen Haghighatkhah</h1>
+            <p className="mode">Frontend developer</p>
+          </div>
+        </div>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+        <Link to="/aboutMe/">About me</Link> <br />
+        <div className='social-wrap'>
+            <h1>L</h1>
+            <h1>L</h1>
+            <h1>L</h1>
+            <h1>L</h1>
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
-export default IndexPage
+export default Home
