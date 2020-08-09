382aeef0b271939a7dcbd7c7122b91bb253a2552
m.haghighakthah <mossii.js@gmail.com>
2020-08-09 11:05:50 +0430

    Provide some structore for develop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
-import './../styles/home.scss';
+import "./../styles/home.scss"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <div className='container'>
-    <SEO title="Mohsen Haghighatkhah Portfolio" />
-    <h1 className='test'>Mohsen Haghighatkhah</h1>
-    <p className='mode'>Frontend developer</p>
-
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-
+  <div className="container">
+       <SEO title="Mohsen Haghighatkhah Portfolio" />
+    <div className="row">
+      <div className='box'></div>
+      <h1 className="name">Mohsen Haghighatkhah</h1>
+      <p className="mode">Frontend developer</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+      <Link to="/aboutMe/">About me</Link> <br />
     </div>
-    <Link to="/aboutMe/">About me</Link> <br />
   </div>
 )
 
